e031c42fb7a4b3d00bb325b1bdf7a0a1ebbb2f21
Matteo Galiazzo <mattegaliz@gmail.com>
2024-04-28 17:16:47 +0200

    i don't even know what i'm doing

--- hw1/main.py
+++ hw1/main.py
@@ -46,34 +46,51 @@ def expgolomb_unsigned(n):
         return headbits + trail_bits
 
 if __name__ == "__main__":
-    img_path = "zanzara_lowres.jpg"
-    img = cv2.imread(img_path)
-    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    rows, cols = img_gray.shape
+    # img_path = "zanzara_lowres.jpg"
+    # img = cv2.imread(img_path)
+    # img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # print(img_gray[:5])
 
-    luminance = 0.2126 * img[:, :, 0] + 0.7152 * img[:, :, 1] + 0.0722 * img[:, :, 2]
+    img_path = "einst.pgm"
+    img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+    img_gray = img
+
+    luminance = 0.2126 * img_gray[:, :] + 0.7152 * img_gray[:, :] + 0.0722 * img_gray[:, :]
 
     # hist contains the counts for each bin in the histogram
     # bins contains the edges of the bins
     hist, bins = np.histogram(luminance.flatten(), 255)
 
-    prob = hist / np.sum(hist)
-    entropy = -np.sum(prob * np.log2(prob))
+    # import matplotlib.pyplot as plt
+    # # Plot the histogram
+    # plt.hist(bins[:-1], bins, weights=hist)
+    # plt.xlabel('Luminance')
+    # plt.ylabel('Frequency')
+    # plt.title('Histogram of Luminance Values')
+    # plt.show()
 
-    print("The entropy of the image is: " + str(entropy) + " bits")
+    # Calculate the probability of each bin
+    prob = hist / hist.sum()
+    # Calculate the entropy
+    entropy = -np.sum(prob[prob != 0] * np.log2(prob[prob != 0]))
+    print("Entropy of the image: ", entropy)
+    # prob = hist / np.sum(hist)
+    # entropy = -np.sum(prob * np.log2(prob))
+
+    # print("The entropy of the image is: " + str(entropy) + " bits")
 
     compressed_img_size = compressed_image_size_bits(img_gray)
     rows, cols = img_gray.shape
     num_pixels = rows * cols
+
     print(
         "The bitrate of the image is: "
         + str(compressed_img_size / num_pixels)
         + "bits per pixel"
     )
 
-    print("The theoretical compression rate is: " + str(8 / entropy) + "\n\n")
-
-    print("the image has " + str(num_pixels) + "pixels")
+    print("The theoretical compression rate is: " + str(8 / entropy))
+    print("the image has " + str(num_pixels) + " pixels")
     # codifica predittiva semplice
 
     linear_image = img_gray.reshape(-1).astype(np.float64)
@@ -83,8 +100,8 @@ if __name__ == "__main__":
     for i in range(1, num_pixels):
         encoded_image[i] = linear_image[i] - linear_image[i - 1]
 
-    cv2.imshow("img", img_gray)
-    cv2.imshow("enc", encoded_image.reshape(rows, cols))
+    # cv2.imshow("img", img_gray)
+    # cv2.imshow("enc", encoded_image.reshape(rows, cols))
 
     decoded_image = np.zeros(num_pixels).astype(np.float64)
     decoded_image[0] = encoded_image[0] + 128
@@ -94,8 +111,8 @@ if __name__ == "__main__":
         decoded_image[i] = buf
 
     decoded_image = decoded_image.reshape(rows, cols).astype(np.uint8)
-    cv2.imshow("dec", decoded_image)
-    cv2.waitKey(0)
+    # cv2.imshow("dec", decoded_image)
+    # cv2.waitKey(0)
 
     # Valutare il numero di bit necessari per codificare l’errore di predizione 
     # y con la codifica Exp Golomb con segno
